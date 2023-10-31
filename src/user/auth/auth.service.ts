9f565ba207835670bc5ee4a73552c9e331c34797
Visinho <Elvisvisinho@rocketmail.com>
2023-10-31 07:14:06 +0100

    Made changes to the prisma schema and validated email

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupProps {
+    email: string;
+    password: string;
+    name: string;
+    phone: string;
+}
 
 @Injectable()
 export class AuthService {
-    signup() {
-        
+  constructor(private readonly prismaService: PrismaService) {}
+
+    async signup({email}: SignupProps) {
+        const userExists = await this.prismaService.user.findFirst({
+            where: {
+                email 
+            }
+        })
+        if(userExists) {
+            throw new ConflictException("User already exists")
+        }
     }
 }
