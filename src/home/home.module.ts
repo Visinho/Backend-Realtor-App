e204840d8383135fadfb684df2e7b90354f46eea
Visinho <Elvisvisinho@rocketmail.com>
2023-11-01 19:21:08 +0100

    Created the Get all homes endpoint

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [HomeController],
   providers: [HomeService]
 })
