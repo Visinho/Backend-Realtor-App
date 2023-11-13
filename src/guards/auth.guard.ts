bd4df025fa7ae410022d09ac5fae9abd0143c70c
Visinho <Elvisvisinho@rocketmail.com>
2023-11-13 11:07:33 +0100

    Ran database request to get user by ID

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,29 +1,49 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import * as jwt from "jsonwebtoken";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import * as jwt from 'jsonwebtoken';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface JWTPayload {
+  name: string;
+  id: number;
+  iat: number;
+  exp: number;
+}
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prismaService: PrismaService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
+    const roles = this.reflector.getAllAndOverride('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
-    constructor(private readonly reflector: Reflector) {}
-    async canActivate(context: ExecutionContext) {
-        const roles = this.reflector.getAllAndOverride("roles", [
-            context.getHandler(),
-            context.getClass(),
-        ]);
-
-        if(roles?.length) {
-            const request = context.switchToHttp().getRequest()
-            const token = request.headers?.authorization?.split("Bearer ")[1]
-            try {
-              const user = await jwt.verify(token, process.env.JWT_TOKEN_KEY);
-              console.log({ user })
-              return true;
-            } catch (error) {
-                return false;
-            }
+    if (roles?.length) {
+      const request = context.switchToHttp().getRequest();
+      const token = request.headers?.authorization?.split('Bearer ')[1];
+      try {
+        const payload = (await jwt.verify(
+          token,
+          process.env.JWT_TOKEN_KEY,
+        )) as JWTPayload;
+        const user = await this.prismaService.user.findFirst({
+          where: {
+            id: payload.id,
+          },
+        });
+        if (!user) {
+          return false;
         }
-        return true;
-
+        if (roles.includes(user.user_type)) return true;
+        return false;
+      } catch (error) {
+        return false;
+      }
     }
+    return true;
+  }
 }
